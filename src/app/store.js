2bcce688d1c6b18102eef0cc625d52dd770de452
chrisDelf <chrisdel@protonmail.com>
2023-08-25 14:08:11 -0400

    init commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/homePage/userSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+
+const persistConfig = {
+  key: 'root', // key for the localStorage
+  storage, // storage mechanism
+};
+const persistedReducer = persistReducer(persistConfig, userReducer);
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedReducer,
   },
 });
 
+export const persistor = persistStore(store);
 export default store;
 
